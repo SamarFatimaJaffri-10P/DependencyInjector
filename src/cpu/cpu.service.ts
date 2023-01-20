7d3d5e378f9d01135dae64d8ba4c699b47c02eda
SamarFatimaJaffri-10P <samar.fatima@10pearls.com>
2023-01-20 13:07:15 +0500

    feat: complete the app and comment extra test file
    comlete the app by adding connecting the parts of the app

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -4,4 +4,9 @@ import { PowerService } from 'src/power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from power service');
+    return a + b;
+  }
 }
